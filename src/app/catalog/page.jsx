b11d7724fc7bf72efd7f83bc5b0e4feb98fecc88
yraffic02 <yraffic02@gmail.com>
2024-07-07 21:53:34 -0300

    chore: add imagens

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -1,27 +1,15 @@
 "use client";
 import { Carousel } from "@/components/Carousel/Carousel";
+import { array1, array2, array3, array4, array5, array6, array7, array8 } from "@/configs/constants";
 
 export default function CatalogPage() {
-  const array1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-  const array2 = ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
-  const array3 = [
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-    "31",
-  ];
+  
   return (
     <div className="flex flex-col items-center gap-2">
       <Carousel images={array1} />
       <Carousel images={array2} />
       <Carousel images={array3} />
+      <Carousel images={array4} />
     </div>
   );
 }
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,14 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import Image from "next/image";
 import { Modal } from "../Modal/Modal";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export const Carousel = ({ images }) => {
+export const Carousel = ({ images, batchSize = 10 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
+  const [loadedImages, setLoadedImages] = useState([]);
+  const [batchIndex, setBatchIndex] = useState(0);
 
   const openModal = (image) => {
     setSelectedImage(image);
@@ -20,12 +22,26 @@ export const Carousel = ({ images }) => {
     setSelectedImage(null);
   };
 
+  const loadBatch = () => {
+    const newImages = images.slice(batchIndex * batchSize, (batchIndex + 1) * batchSize);
+    setLoadedImages((prevImages) => [...prevImages, ...newImages]);
+  };
+
+  useEffect(() => {
+    loadBatch();
+  }, [batchIndex]);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    afterChange: (current) => {
+      if (current + 3 >= loadedImages.length && loadedImages.length < images.length) {
+        setBatchIndex((prevIndex) => prevIndex + 1);
+      }
+    },
     responsive: [
       {
         breakpoint: 768,
@@ -40,23 +56,15 @@ export const Carousel = ({ images }) => {
   return (
     <div className="w-[90vw] mx-auto">
       <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index} className="p-2">
+        {loadedImages.map((image, index) => (
+          <div key={index} className="p-1">
             <Image
-              src={`/vestidos/${image}.webp`}
+              src={`/vestidos/${image}.jpeg`}
               height={250}
               width={250}
               alt={image}
-              className="object-cover relative"
+              className="object-cover"
               onClick={() => openModal(image)}
-              priority
-            />
-            <Image
-              src="/logo.png"
-              className="absolute bottom-5"
-              height={30}
-              width={30}
-              alt="logo"
               priority
             />
           </div>
